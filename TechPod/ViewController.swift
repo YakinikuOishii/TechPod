2d3e7619c23e41414eee523e4edd472b86376120
Miku <fum_o3o7@ezweb.ne.jp>
2019-09-15 11:38:50 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by 笠原未来 on 2019/09/15.
+//  Copyright © 2019 笠原未来. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
